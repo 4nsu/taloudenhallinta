bca8243cb8e5f4f354bd086bbdaf6eb1e85cdbb7
ansu <annastiina.koivu@gmail.com>
2025-01-15 13:53:36 +0200

    lisää ButtonContainer-komponentin

---
+++ src/shared/buttons/ButtonContainer.jsx
@@ -0,0 +1,18 @@
+import styles from './Buttons.module.scss'
+import clsx from 'clsx'
+
+function ButtonContainer({ className, children, ...props }) {
+    return (
+        <div 
+            className={clsx(
+                styles.button_container,
+                className
+            )}
+            {...props} 
+        >
+            { children }
+        </div>
+    )
+}
+
+export { ButtonContainer as default, ButtonContainer }
