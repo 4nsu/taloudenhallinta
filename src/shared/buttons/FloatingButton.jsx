bd2e7f27f1731ef50bd154013e38037daf9c1390
ansu <annastiina.koivu@gmail.com>
2025-01-15 13:36:48 +0200

    lisää FloatingButton-komponentin

---
+++ src/shared/buttons/FloatingButton.jsx
@@ -0,0 +1,17 @@
+import styles from './Buttons.module.scss'
+import clsx from 'clsx'
+import Button from './Button.jsx'
+
+function FloatingButton({ className, ...props }) {
+    return (
+        <Button
+            className={clsx(
+                styles.button_floating,
+                className
+            )}
+            {...props}
+        />
+    )
+}
+
+export { FloatingButton as default, FloatingButton }
