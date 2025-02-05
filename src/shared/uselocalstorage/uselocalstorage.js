76dcda4aaec96adc1f986d0d391f53e1e0a2a0f4
ansu <annastiina.koivu@gmail.com>
2025-02-05 22:14:25 +0200

    lisää useLocalStorage-funktioon liittyvät JSDocs-dokumentaatiot

--- src/shared/uselocalstorage/uselocalstorage.js
+++ src/shared/uselocalstorage/uselocalstorage.js
@@ -1,14 +1,65 @@
 import { useEffect, useState } from "react"
 
-// Muuttaa muuttujan JSON-merkkijonoksi
+/**
+ * Muuntaa muuttujan JSON-merkkijonoksi.
+ * @param {any} value - Muutettava arvo.
+ * @returns {string} JSON-merkkijono.
+ */
+
 const decode = (value) => {
     return JSON.stringify(value)
 }
 
-// Purkaa JSON-merkkijonon muuttujaksi
+/**
+ * Purkaa JSON-merkkijonon takaisin alkuperäiseksi muuttujaksi.
+ * @param {string} value - JSON-merkkijono.
+ * @returns {any} Palautettu arvo.
+ */
+
 const encode = (value) => {
     return JSON.parse(value)
 }
+
+/**
+ * @typedef  {Array} useLocalStorageArray
+ * @property {any} value
+ *           localStorageen tallennettu tilamuuttuja, tai oletusarvo.
+ * @property {function} setValue
+ *           Päivittää localStoragessa olevan tilamuuttujan arvon.
+ *           Funktio on useState-hookin palauttama set-funktio.
+ * @property {function} resetValue
+ *           Palauttaa oletusarvot.
+ */
+
+/**
+ * React Hook, joka tallentaa ja lukee tilamuuttujan arvon localStoragesta.
+ * 
+ * @example
+ * import useLocalStorage from './uselocalstorage.js'
+ * 
+ * // Alustetaan tilamuuttuja, joka hakee arvonsa localStoragesta
+ * // avaimella 'dataKey'. Oletusarvoksi annetaan tyhjä taulukko.
+ * const [data, setData] = useLocalStorage('dataKey',[])
+ *
+ * // Päivitetään localStoragessa olevaa arvoa.
+ * const update = (value) => {
+ *   let copy = data.slice()
+ *   copy.push(value)
+ *   setData(copy)
+ * }
+ * 
+ * // Luetaan localStoragessa oleva arvo.
+ * console.log(data)
+ * 
+ * @param   {string} key
+ *          localStorageen tallennettavan arvon avain.
+ * @param   {any} defaultState
+ *          Oletusarvo, jos avainta ei löydy localStoragesta.
+ * @returns {useLocalStorageArray}
+ *          localStorageen tallennettu arvo (tai oletusarvo),
+ *          sekä tallennuksen funktiot taulukkona.
+ * 
+ */
 
 const useLocalStorage = (key, defaultState) => {
 
